da501967cc0b7b1cd788f3ea71406dda1fbad614
Carrot Soup <11361651+dlhristov@users.noreply.github.com>
2018-09-21 20:43:48 -0400

    rudimentary comment form validation

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -12,14 +12,42 @@ firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
-
 
     var contactName = $("#contact-name").val().trim();
     var contactEmail = $("#contact-email").val().trim();
     var contactComment = $("#contact-comment").val().trim();
+    var validEmail = false;
+    var validComment = false;
+    var validName = false;
 
+    if (contactName.length == 0) {
+        console.log("name empty");
+        $("#name-label").css("color", "#FF0000");
+        $("#name-label").html("Your Name (*field required)");
+    } else {
+        validName = true;
+    }
+
+    if (contactComment == 0) {
+        console.log("comment empty")
+        $("#comment-label").css("color", "#FF0000");
+        $("#comment-label").html("Question / Comment (*field required)");
+    } else {
+        validComment = true;
+    }
+
+    if (!checkEmail(contactEmail)) {
+        console.log("not an email");
+        $("#email-label").css("color", "#FF0000");
+        $("#email-label").html("Your E-mail Address (*invalid e-mail)");
+    } else {
+        validEmail = true;
+    }
+
+
+    if(validName && validEmail && validComment){
     console.log(contactName);
     console.log(contactEmail);
     console.log(contactComment);
@@ -30,7 +58,15 @@ $("#submit").on("click", function(event) {
         email: contactEmail,
         comment: contactComment
     });
+}
+
+
 
 
 
 });
+
+function checkEmail(email) {
+    var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return emailRegex.test(email);
+}
